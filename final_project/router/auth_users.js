ada2c192479255b8b7444cc4882851b713dc0c74
Hammu0604 <hammu0604@gmail.com>
2026-05-22 03:36:46 -0400

    Updated auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,7 @@ const regd_users = express.Router();
 
 let users = [];
 
+
 // Check username exists or not
 const isValid = (username) => {
 
@@ -21,14 +22,17 @@ const isValid = (username) => {
   }
 };
 
+
 // Check login credentials
 const authenticatedUser = (username, password) => {
 
   let validusers = users.filter((user) => {
+
     return (
       user.username === username &&
       user.password === password
     );
+
   });
 
   if (validusers.length > 0) {
@@ -37,6 +41,7 @@ const authenticatedUser = (username, password) => {
     return false;
   }
 };
+
 
 // REGISTER USER
 regd_users.post("/register", (req, res) => {
@@ -54,7 +59,7 @@ regd_users.post("/register", (req, res) => {
       });
 
       return res.status(200).json({
-        message: "User successfully registered. Now you can login"
+        message: "Customer successfully registered. Now you can login"
       });
 
     } else {
@@ -62,13 +67,17 @@ regd_users.post("/register", (req, res) => {
       return res.status(404).json({
         message: "User already exists!"
       });
+
     }
+
   }
 
   return res.status(404).json({
     message: "Unable to register user."
   });
+
 });
+
 
 // LOGIN USER
 regd_users.post("/login", (req, res) => {
@@ -81,6 +90,7 @@ regd_users.post("/login", (req, res) => {
     return res.status(404).json({
       message: "Error logging in"
     });
+
   }
 
   if (authenticatedUser(username, password)) {
@@ -90,7 +100,9 @@ regd_users.post("/login", (req, res) => {
         data: password
       },
       'access',
-      { expiresIn: 60 * 60 }
+      {
+        expiresIn: 60 * 60
+      }
     );
 
     req.session.authorization = {
@@ -98,24 +110,32 @@ regd_users.post("/login", (req, res) => {
       username
     };
 
-    return res.status(200).send("Customer successfully logged in.");
+    return res.status(200).json({
+      message: "Customer successfully logged in"
+    });
+
   }
 
   return res.status(208).json({
     message: "Invalid Login. Check username and password"
   });
+
 });
+
 
 // ADD OR MODIFY REVIEW
 regd_users.put("/auth/review/:isbn", (req, res) => {
 
   const isbn = req.params.isbn;
-  const review = req.query.review;
+
+  const review = req.body.review;
 
   const username = req.session.authorization.username;
 
   if (!books[isbn].reviews) {
+
     books[isbn].reviews = {};
+
   }
 
   books[isbn].reviews[username] = review;
@@ -123,7 +143,9 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({
     message: "Review added/modified successfully"
   });
+
 });
+
 
 // DELETE REVIEW
 regd_users.delete("/auth/review/:isbn", (req, res) => {
@@ -139,12 +161,15 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
     return res.status(200).json({
       message: "Review deleted successfully"
     });
+
   }
 
   return res.status(404).json({
     message: "Review not found"
   });
+
 });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
